c69f34122ba98e5a470017f480bf7f2f227d932c
ahmad-imtiyaz <imtiyaznajih8@gmail.com>
2025-09-11 08:32:40 +0700

    Revert "revisi 123"
    This reverts commit a47bbe8949e05303395ea708aca5448430208326.

--- src/components/FormAset.js
+++ src/components/FormAset.js
@@ -184,8 +184,12 @@ const FormAset = ({
   };
 
   const statusOptions = [
-    { value: "Dimiliki", label: "Dimiliki" },
-    { value: "Dikuasai", label: "Dikuasai" },
+    { value: "Dimiliki/Dikuasai", label: "Dimiliki/Dikuasai" },
+    {
+      value: "Tidak Dimiliki/Tidak Dikuasai",
+      label: "Tidak Dimiliki/Tidak Dikuasai",
+    },
+    { value: "Lain-lain", label: "Lain-lain" },
   ];
 
   // Helper function untuk mendapatkan nilai luas yang sedang aktif
