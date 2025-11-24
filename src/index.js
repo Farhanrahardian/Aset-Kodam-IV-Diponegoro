78522503c4c85bcdd3fdd45e0b78d4607f3e5721
ahmad-imtiyaz <imtiyaznajih8@gmail.com>
2025-11-24 16:37:06 +0700

    menambahkan PWA masih error logo

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration"; // ✅ Import service worker
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -12,7 +13,7 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+// ✅ Daftarkan service worker supaya PWA bisa offline
+serviceWorkerRegistration.register();
